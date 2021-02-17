291ef0d87cc3fa70941d9c013b05f66859d985b4
Thanaya <Thanaya@Thanaya-PC>
2021-02-17 15:42:01 +0530

    Wh-8850 First time changes for testApp

---
+++ src/main/java/com/example/demo/MessageRunner.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+     System.out.println("Welcome to message runner");
+
+	}
+
+}
---
+++ src/main/java/com/example/demo/SpringbootTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestAppApplication.class, args);
+	}
+
+}
