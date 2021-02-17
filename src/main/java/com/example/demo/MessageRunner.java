feb40eb280c88c44797278af5924a3dba7cb9a8d
Thanaya <Thanaya@Thanaya-PC>
2021-02-17 16:19:38 +0530

    Wh-6888Thanay Reddy modified

--- src/main/java/com/example/demo/MessageRunner.java
+++ src/main/java/com/example/demo/MessageRunner.java
@@ -7,8 +7,15 @@ public class MessageRunner implements CommandLineRunner {
 
 	//modified by sudhakar login page 17/02/2021
 	public void run(String... args) throws Exception {
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
      System.out.println("Welcome to message runner");
+     System.out.println("addition of two numbers is::"+c);
 
+     System.out.println("welcome to sudhakar");
 	}
 
 }
